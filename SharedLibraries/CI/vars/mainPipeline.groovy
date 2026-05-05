b8e119f68c8013b6c11b5fa7e7e9b5e0c2334a46
priera12 <rierapablo12@gmail.com>
2026-05-05 01:16:04 -0300

    Add Shared Libraries

--- SharedLibraries/CI/vars/mainPipeline.groovy
+++ SharedLibraries/CI/vars/mainPipeline.groovy
@@ -28,8 +28,10 @@ def call(Map config = [:]){
 //            }
             stage('Build Image'){
                 steps{
-                    def destination = "${env.IMAGE_DESTINATION}"
-                    kanikoBuilder(destination)
+                    step{
+                        def destination = "${env.IMAGE_DESTINATION}"
+                        kanikoBuilder(destination)
+                    }
                 }
             }
             stage('Deploy to Minikube'){
