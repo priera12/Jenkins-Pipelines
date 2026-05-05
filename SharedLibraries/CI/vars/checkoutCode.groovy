f03e4ed2e3c8c1f8b6b276ba22f2d84c3988c5ad
priera12 <rierapablo12@gmail.com>
2026-05-05 00:38:32 -0300

    Add Shared Libraries

--- SharedLibraries/CI/vars/checkoutCode.groovy
+++ SharedLibraries/CI/vars/checkoutCode.groovy
@@ -1,16 +1,16 @@
 def call(Map args = [:]) {
 
-    def repoName = args.repoName
+    def appName = args.IMAGE_NAME
     def branchName = args.branchName ?: 'main'
-    def credencials = args.credencialsId ?: 'Jenkins-pipeline'
+    def credencials = 'Jenkins-pipeline'
     def baseUrl = args.baseUrl ?: "https://github.com/priera12"
 
-    echo "Iniciando Checkout del repositorio: ${repoName} (Rama: ${branchName})"
+    echo "Iniciando Checkout del repositorio: ${baseUrl} (Rama: ${branchName})"
 
     checkout([$class: 'GitSCM', 
         branches: [[name: branchName]], 
         userRemoteConfigs: [[
-            url: "${baseUrl}/${repoName}.git",
+            url: "${baseUrl}/${appName}.git",
             credentialsId: credentials
         ]]
     ])    
