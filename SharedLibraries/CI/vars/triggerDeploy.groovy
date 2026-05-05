dbefe5f96d9003636bc01e18c8e7887f96903a38
priera12 <rierapablo12@gmail.com>
2026-05-05 02:22:51 -0300

    Add Shared Libraries

--- SharedLibraries/CI/vars/triggerDeploy.groovy
+++ SharedLibraries/CI/vars/triggerDeploy.groovy
@@ -7,13 +7,13 @@ def call (args = [:]){
 
     if (deployEnabled){
 
-        echo "Depliegue Habilitado"
+        echo "Despliegue Habilitado"
 
-        build job: "Despliegue/Deploy_${IMAGE_NAME}/main", 
+        build job: "Despliegue/Deploy_${args.appName}/main", 
             parameters: [
-                string(name: 'APP_NAME', value: appName),
-                string(name: 'VERSION', value: version),
-                string(name: 'BRANCH', value: branch)
+                string(name: 'APP_NAME', value: "${args.appName}"),
+                string(name: 'VERSION', value: "${args.version}"),
+                string(name: 'BRANCH', value: "${args.branch}")
             ], 
             wait: true
 
